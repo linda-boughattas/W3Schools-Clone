d11cbf12997f8218fb19222a13a4d79e2f427d86
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-08-28 13:18:17 +0200

    added some adjustments to navbar elements

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -3,19 +3,19 @@ import W3Schools from './assets/W3Schools_logo.png'
 
 export default function Navbar(){
     return(
-            <nav class="navbar navbar-default">
-                <div class="container-fluid">
+            <nav class="navbar-default">
                     <div class="navbar-header">
                     <a class="navbar-brand" href="#"><img src={W3Schools}></img></a>
                     </div>
-                    <ul class="nav navbar-nav">
+                    <ul class="navbar-nav">
                     <li><a href="#">Tutorials</a><i class="fa fa-caret-down"></i></li>
                     <li><a href="#">References</a><i class="fa fa-caret-down"></i></li>
                     <li><a href="#">Exercies</a><i class="fa fa-caret-down"></i></li>
                     </ul>
+                <div className="navbar-login">
+                    <a href="#">Paid Courses</a>
+                    <button>Log in</button>
                 </div>
-                <a href="#">Paid Courses</a>
-                <button>Log in</button>
             </nav>
     )
 }

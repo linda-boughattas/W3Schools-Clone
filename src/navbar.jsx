376cefed031421d97197f6fe69f3297335695257
Linda Boughattas <linda.boughattas@enicar.ucar.tn>
2024-08-29 15:43:44 +0200

    added some changes to the styling of the navbar

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -10,11 +10,11 @@ export default function Navbar(){
                     <ul className="navbar-nav">
                     <li><a href="#">Tutorials</a><i className="fa fa-caret-down"></i></li>
                     <li><a href="#">References</a><i className="fa fa-caret-down"></i></li>
-                    <li><a href="#">Exercices</a><i className="fa fa-caret-down"></i></li>
+                    <li><a href="#">Exercies</a><i className="fa fa-caret-down"></i></li>
                     </ul>
                 <div className="navbar-login">
-                    <button className="button white">Paid Courses</button>
-                    <button className="button green">Log in</button>
+                    <button className="button white"><a target="_blank" href="https://campus.w3schools.com/collections/course-catalog">Paid Courses</a></button>
+                    <button className="button green"><a href="https://profile.w3schools.com/login?redirect_url=https%3A%2F%2Fwww.w3schools.com%2F">Log in</a></button>
                 </div>
             </nav>
     )
